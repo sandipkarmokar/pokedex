af48d45bbc5ff719b1a7955a17852674e35df54e
sandipkarmokar <skarmokar987@gmail.com>
2023-12-28 23:19:16 +0530

    made all components

--- app/Components/MainDisplay.jsx
+++ app/Components/MainDisplay.jsx
@@ -1,9 +1,14 @@
 "use client";
-
 import React from "react";
+import PokemonCard from "./PokemonCard";
 
 const MainDisplay = () => {
-  return <div>MainDisplay</div>;
+  return (
+    <div>
+      MainDisplay
+      <PokemonCard></PokemonCard>
+    </div>
+  );
 };
 
 export default MainDisplay;
---
+++ app/Components/PokemonCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PokemonCard = () => {
+  return <div className="text-yellow-500">PokemonCard</div>;
+};
+
+export default PokemonCard;
