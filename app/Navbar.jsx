12e63428ec82db277833d625bb3467f2b742ca2d
sandipkarmokar <skarmokar987@gmail.com>
2024-01-04 11:10:18 +0530

    added pokeball icon and github icon

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -1,14 +1,32 @@
 import React from "react";
+import Image from "next/image";
+
+// const pokeball = "../public/pokeball.svg";
 
 const Navbar = () => {
   return (
     <div className="bg-red-600 sm:h-24 h-16 p-8">
       <div className="container mx-auto flex  items-center justify-between text-blue-200 h-full">
-        <div>
-          <p>Pokedex</p>
+        <div className="flex items-center">
+          <Image
+            src="/pokeball.svg"
+            width={44}
+            height={44}
+            alt="pokeball img"
+            className="sm:mr-6 mr-2"
+          ></Image>
+          <p className="sm:text-[40px] text-[24px] text-red-400 font-bold">
+            Pokedex
+          </p>
         </div>
         <div>
-          <p>Github</p>
+          <Image
+            src="/github.svg"
+            width={44}
+            height={44}
+            alt="github"
+            // className="sm:mr-6 mr-2"
+          ></Image>
         </div>
       </div>
     </div>
